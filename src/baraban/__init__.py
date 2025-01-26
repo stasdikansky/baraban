d5afed04e1dcdb954b8fdadfba5d37bd2be57de8
stasdikansky <stas.dikansky@gmail.com>
2025-01-26 14:11:22 +0100

    Added .gitignore

--- src/baraban/__init__.py
+++ src/baraban/__init__.py
@@ -4,6 +4,6 @@ A/B Testing Library
 
 from .baraban import ABTestConfig, ABTester, SampleSizeConfig
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __all__ = ["ABTester", "ABTestConfig", "SampleSizeConfig"] 
