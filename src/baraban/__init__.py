8c88a288047e39c204539ceee62d2cd2eef34438
stasdikansky <stas.dikansky@gmail.com>
2025-02-19 20:22:46 +0100

    Rounding to 5 decimal places when calculating means.

--- src/baraban/__init__.py
+++ src/baraban/__init__.py
@@ -4,6 +4,6 @@ A/B Testing Library
 
 from .baraban import ABTestConfig, ABTester, SampleSizeConfig
 
-__version__ = "0.1.7"
+__version__ = "0.1.8"
 
 __all__ = ["ABTester", "ABTestConfig", "SampleSizeConfig"] 
